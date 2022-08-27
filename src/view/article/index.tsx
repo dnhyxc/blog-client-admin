b00d77f797773263c2a8f1c769b34db82277e689
dnhyxc <2640559435@qq.com>
2022-08-28 03:25:44 +0800

    feat：init

--- src/view/article/index.tsx
+++ src/view/article/index.tsx
@@ -34,8 +34,6 @@ const data = [
 const Article: React.FC = () => {
   const { showAlert, toLogin, onCloseAlert } = useLoginStatus();
 
-  console.log(showAlert, 'showAlert');
-
   return (
     <div className={styles.ArticleContainer}>
       {showAlert && <MAlert onClick={toLogin} onClose={onCloseAlert} />}
